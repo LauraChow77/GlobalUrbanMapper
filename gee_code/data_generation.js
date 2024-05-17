3fd05b8fce106afa45cb7113a612ea65d804c533
LauraChow77 <114505027+LauraChow77@users.noreply.github.com>
2024-05-17 20:50:45 +0800

    Update data_generation.js

--- gee_code/data_generation.js
+++ gee_code/data_generation.js
@@ -20,7 +20,7 @@ var userVars = {
   geometry: geometry,
   start_date: '2019-01-01',
   end_date: '2020-01-01',
-  name: 'HongKong' // (export image name)
+  name: '' // (export image name)
 };
 
 // -----------------------------------------------------------------------------
